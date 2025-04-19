6950c7d5178d6690972369aec87bbb31a2dfa76f
ZenitsuTHB <starxyz12@gmail.com>
2025-04-19 11:58:15 +0200

    tada same exercise but new way of doing the remove space

--- javascript/day17/Ex03/datalist.js
+++ javascript/day17/Ex03/datalist.js
@@ -10,20 +10,19 @@
 
 document.querySelector('button').addEventListener('click', sendBox)
 
-function parserName(oldname) {
-  let tmpName = ' '
-  let newName = oldname.trim()
+function parserName(oldName) {
+  oldName = oldName.trim()
+  let newName = ''
 
-  for( let i = 0; i < newName.size; i++)
+  for( let i = 0; i < oldName.length; i++)
   {
-    if (tmpName[i] == ' ')
+    if (oldName[i] === ' ')
     {
-      i++; 
-      continue;
+      newName += oldName[i];
+      while (oldName[i] === ' ')
+        i++;  
     }
-    if (i < tmpName.size)
-      newName += tmpName
-
+    newName += oldName[i];
   }
   return newName;
 }
@@ -32,7 +31,6 @@ function sendBox() {
 
   let name = document.querySelector('#name').value
   name = parserName(name)
-  alert(name)
   if(!name) { 
     alert('Sorry name is mandatory !') 
     return 
@@ -40,8 +38,85 @@ function sendBox() {
 
   let material = document.querySelector('#material').value
   let dimension = document.querySelector('[name=dimension]:checked').value
-  let comment = document.querySelector('')
+  let comment = document.querySelector('#comments').value
 
-  let msg = name + 'ha pedido una caja de ' + material + dimension + comment 
+  let msg = name + ' ha pedido una caja de [' + material + '] con unas dimensiones [' + dimension + '].'+ comment 
   document.querySelector('#message').value = msg
 }
+
+//OTHER WAYS
+//document.querySelector('button').addEventListener('click', sendRequest)
+  //document.querySelector('button').onclick = sendRequest
+
+    /*function eliminarEspacios(texto) {
+    
+    while (texto.indexOf('  ') >=0) {
+    texto = texto.replace('  ', ' ')
+    }
+    return texto
+    }
+      function eliminarEspaciosConsecutivos(texto) {
+      while (texto.includes("  ")) {
+        texto = texto.split("  ").join(" ");
+      }
+      return texto.trim();
+    }
+
+      function eliminarEspaciosConsecutivos(texto) {
+    return texto.split(/\s+/).join(' ').trim();
+      }
+    }
+    
+    function trimmer(text) {
+ 
+      console.log('hola esta es trimmer')
+      let trimmed = text.trim()
+      let res = '';
+      let flag = false
+      
+     for(let i = 0; i < trimmed.length; i++)
+      {
+        if (trimmed[i] == ' ')
+            flag = true
+        if (flag === true)
+        {
+          res += ' ';
+          while (trimmed[i] == ' ')
+            i++
+          flag = false
+        }
+        if (i < trimmed.length)
+          res += trimmed[i]
+      }
+      console.log('this is the res \n',res)
+      return(res) 
+    }
+    
+    function sendRequest () {
+
+  
+      document.querySelector('#message').value = ''
+      let name = document.querySelector("#name").value
+      let material = document.querySelector('#material').value
+    
+      let dimension = document.querySelector('[name=dimension]:checked').value
+      if (!dimension){
+        window.alert('Dimension is null or undefined')
+        return
+      }
+      console.log('entrada: \n', name)
+      name = trimmer(name)
+      console.log('salida: \n', name)
+
+      let comments = document.querySelector('#comments').value.trim()
+
+      if(name === '')
+      {
+        window.alert('Name is mandatory !')
+        return        
+      }
+      let msg = name + ' ha pedido una caja de ' + material + ' con unas dimensiones ' + dimension + '. ' + comments
+
+      document.querySelector('#message').value = msg
+    }
+    */
